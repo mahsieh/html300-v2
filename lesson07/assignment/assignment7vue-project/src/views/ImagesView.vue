15699b3071f5d3b7e5068f6129b3ff24aa57e375
mahsieh <michaelhsieh.alt@gmail.com>
2023-12-17 05:10:22 -0800

    imported assets from previous assignment to assets folder (images); created additional pages under views to match functionality from previous assignment; images are responsively displayed using v-for in ImagesView; v-if and v-else are used in AboutView, and AccordionView also uses v-for to display bootstrapped accordion, although this did not work properly. All repeated content are pulled from .vue files script section and built using v-fors in their respective pages.

---
+++ lesson07/assignment/assignment7vue-project/src/views/ImagesView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="images">
+        <h1>This is an images page</h1>
+
+        <!--iterate through images object and display each image individually-->
+        <div v-for="image in images">
+            <!--use bootstrap to have responsive images-->
+            <img class="img-fluid" :src="image.src" :title="image.caption">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            /*array of image objects from previous assignment*/
+            images: [
+                {
+                    src: "/src/assets/travel.jpg",
+                    caption: "Tooltipped image depicting air travel to various locations"
+                },
+                {
+                    src: "/src/assets/urban_vacation.jpg",
+                    caption: "Tooltipped image of Paris near Eiffel Tower"
+                },
+                {
+                    src: "/src/assets/tropical_vacation.jpg",
+                    caption: "Tooltipped image of a tropical island with pier"
+                }
+            ]
+        }
+    }
+}
+</script>
+  
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
